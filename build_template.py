da5340bc1e39b9f309b157acd0e4e35a98ac4c1b
Noah S <technicaath@gmail.com>
2023-08-30 22:00:15 -0400

    build annotate file

---
+++ build_template.py
@@ -0,0 +1,51 @@
+import cv2
+import os
+import pandas as pd
+
+def create_annotation_template(video_path, output_folder):
+    # Create a video capture object
+    cap = cv2.VideoCapture(video_path)
+    
+    # Check if the video was opened successfully
+    if not cap.isOpened():
+        print(f"Error: Unable to open video file: {video_path}")
+        return
+    
+    frame_timestamps = []
+    frame_number = 0  # Track frame number for timestamp calculation
+    fps = cap.get(cv2.CAP_PROP_FPS)  # Get frames per second
+    
+    # Iterate over the frames just to calculate timestamps
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        
+        frame_timestamps.append(frame_number / fps)  # Store the timestamp
+        frame_number += 1
+
+    cap.release()
+
+    # Convert frame timestamps into hh:mm:ss format
+    timestamps = [
+    "{:02d}:{:02d}:{:02d}.{:03d}".format(
+        int(t // 3600),
+        int((t % 3600) // 60),
+        int(t % 60),
+        int((t % 1) * 1000)
+    )
+    for t in frame_timestamps
+]
+    
+    # Create a DataFrame with timestamps and empty "head movement" column
+    df = pd.DataFrame({
+        'Timestamp': timestamps,
+        'Head Movement': [''] * len(timestamps)
+    })
+
+    # Save the DataFrame to an Excel file
+    df.to_excel(os.path.join(output_folder, 'annotation_template_test.xlsx'), index=False)
+
+video_path = 'Recordings\VR2_trim.mp4'
+output_folder = 'Recordings\Frames'
+create_annotation_template(video_path, output_folder)
